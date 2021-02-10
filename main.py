1b0ab282a030b078e058d3e8015ccc22fb4ea2bf
atrox3d <atrox3d@gmail.com>
2021-02-10 15:15:28 +0100

    final message

--- main.py
+++ main.py
@@ -16,3 +16,6 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("you have completed the quiz")
+print(f"your final score was: {quiz.score}/{quiz.question_number}")
